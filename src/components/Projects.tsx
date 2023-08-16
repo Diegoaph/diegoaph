4d8b0890e095f9e151333cf1ea178a5974e1e64f
Diegoaph <Diegoaph2018@gmail.com>
2023-08-15 20:49:11 -0500

    imgs sunset

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -31,16 +31,16 @@ const Projects: React.FC<{}> = () => {
 
 const itemData = [
     {
-        img: "https://media.licdn.com/dms/image/D4E22AQEQZl1XQQXfmA/feedshare-shrink_1280/0/1690911793910?e=1694044800&v=beta&t=StpDteUQYaDSJgiLARX0C0sFcAGTexx1mQD9E5MEEeE",
+        img: "https://media.licdn.com/dms/image/D4E22AQGB_ikLBJWfxA/feedshare-shrink_1280/0/1692148763769?e=1695254400&v=beta&t=Bu_AkVozFAXYP_cDPH3A_wfG6hNDmAZPxvSkUZEYwSU",
         title: "Sunset Sands Hotel",
         author: "Booking engine for hotels",
-        URL: "https://devdiego-bookingengine.vercel.app/",
+        URL: "https://devdiego-bookings.vercel.app/",
     },
     {
         img: "https://media.licdn.com/dms/image/D4E22AQGowmQwv0dZOw/feedshare-shrink_1280/0/1690911793922?e=1694044800&v=beta&t=DnwiNGiZeVWcPHWUz1VjdEuxct3OT3WdCVQeGyTYKnU",
         title: "GamingHub",
         author: "Videogames atlas",
-        URL: "https://devdiego-gaminghub.vercel.app/",
+        URL: "https://devdiego-videogames.vercel.app/",
     },
     {
         img: "https://media.licdn.com/dms/image/D4E22AQF-VPcR1z07wg/feedshare-shrink_800/0/1690605400062?e=1693440000&v=beta&t=PEHS-Re3bgHA7Tq5fuTP8FDTNmd_gUaljaVkyyJycjA",
