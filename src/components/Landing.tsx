a3e3329b754c38a76f4a98ef781f6e792a96a171
Diegoaph <Diegoaph2018@gmail.com>
2023-08-29 08:55:30 -0500

    width4

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -76,7 +76,7 @@ const Landing: React.FC<{ lang: boolean }> = ({ lang }) => {
                 <Stack
                     direction="row"
                     spacing={1}>
-                    <section className="aside">
+                    <div className="aside">
                         <Button
                             variant="outlined"
                             onClick={() => handleScrollToSection("top")}>
@@ -101,7 +101,7 @@ const Landing: React.FC<{ lang: boolean }> = ({ lang }) => {
                             onClick={() => handleScrollToSection("about")}>
                             {lang ? "About Me" : "Sobre Mí"}
                         </Button>
-                    </section>
+                    </div>
                 </Stack>
             </Grid>{" "}
             <Container>
