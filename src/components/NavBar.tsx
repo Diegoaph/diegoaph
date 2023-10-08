9e8573920608df5cf24b60e1234f66df43239384
Diegoaph <Diegoaph2018@gmail.com>
2023-10-08 13:37:09 -0500

    newcves

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -176,7 +176,7 @@ export const NavBar: React.FC<NavBarProps> = ({ lang, setLang }) => {
                                         download="Diego-Pacheco-full-stack-resume"
                                         target="blank">
                                         <Button variant="outlined">
-                                            {lang ? "CV🡣" : "CV🡣"}
+                                            {lang ? "CV↓" : "CV↓"}
                                         </Button>
                                     </a>{" "}
                                     <NavLink
